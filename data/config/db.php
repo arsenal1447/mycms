6dd05c7e1321c412bdf66fbd889a39cf58c21fc9
xinxin <xinxin@rrkd.cn>
2016-04-05 17:13:38 +0800

    填写数据库信息

--- data/config/db.php
+++ data/config/db.php
@@ -1,1 +1,10 @@
-<?php return [];?>
+<?php return array (
+  'class' => 'yii\\db\\Connection',
+  'dsn' => 'mysql:host=localhost;dbname=lulu',
+  'username' => 'root',
+  'password' => '123456',
+  'charset' => 'utf8',
+  'tablePrefix' => 'lulu_',
+  'enableSchemaCache' => true,
+  'schemaCache' => 'schemaCache',
+) ;?>
